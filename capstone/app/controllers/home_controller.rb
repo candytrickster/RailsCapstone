0bad675674739b6330b16a7e7754f38f1473a33a
Tracie Wamsley <traciewamsley@gmail.com>
2016-01-20 13:53:29 -0700

    countdown works! cannot edit countdown yet

--- capstone/app/controllers/home_controller.rb
+++ capstone/app/controllers/home_controller.rb
@@ -5,18 +5,25 @@ class HomeController < ApplicationController
       cookName = cookies[:user_name]
       cookFname = cookies[:user_fname]
       @message = 'Congratulations ' + cookName + ' and ' + cookFname + '!'
-      @date = cookies[:user_date]
-      @current_date = DateTime.now.strftime('%m/%d/%Y')
+      @date = ''+cookies[:user_date]
+      @current_date = DateTime.now()
       # @d = Date.strptime(@date, '%m/%d/%Y')
       @d = Date.parse(@date)
-      @d = @d.strftime('%m/%d/%Y')
+      # @d = @d.strftime('%m/%d/%Y')
+      @dd = cookies[:user_date][0..1]
+      @mm = cookies[:user_date][3..4]
+      @yyyy = cookies[:user_date][6..9]
       @log = true
+      # parseInts(@dd, @mm, @yyyy)
+      @what = @date.is_a?(String)
     end
 
   end
 
-  def clearCookies
-    cookies.delete :user_name
+  def parseInts(dd,mm,yyyy)
+    @dd = Integer(dd)
+    @mm = Integer(mm)
+    @yyyy = Integer(yyyy)
   end
 
 end
