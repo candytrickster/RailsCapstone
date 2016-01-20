59d6dd26db90de77a6710e7e5ed4d495da0b0f57
Tracie Wamsley <traciewamsley@gmail.com>
2016-01-20 15:36:30 -0700

    edit countdown layout and functionality works

--- capstone/app/controllers/home_controller.rb
+++ capstone/app/controllers/home_controller.rb
@@ -2,16 +2,42 @@ class HomeController < ApplicationController
 
   def home
     if(!cookies[:user_name].blank?)
-      cookName = cookies[:user_name]
-      cookFname = cookies[:user_fname]
-      @message = 'Congratulations ' + cookName + ' and ' + cookFname + '!'
+      @message = 'Congratulations ' + cookies[:user_name] + ' and ' + cookies[:user_fname] + '!'
       @date = ''+cookies[:user_date]
       @log = true
     end
 
   end
 
-  def editCountdown
+  def edit
+    if(!cookies[:user_name].blank?)
+      @message = 'Edit Wedding Date'
+      @log = true
+    end
+  end
+
+  def updateDate
+    # if(!cookies[:user_name].blank?)
+    #   @message = 'Congratulations ' + cookies[:user_name] + ' and ' + cookies[:user_fname] + '!'
+      # # if params[:date] ==
+      # cookies[:user_date] = params[:date]
+      # @log = true
+      # respond_to do |format|
+      #   format.html { redirect_to '/home'}
+      # end
+    # end
+    @message = 'Edit Wedding Date'
+    @log = true
+    @user = User.find(cookies[:user_id])
+      @user.update_attribute(:wedDate, params[:date])
+      if @user.save
+        respond_to do |format|
+          format.html { redirect_to '/home'}
+          cookies[:user_date] = @user.wedDate
+        end
+      else
+        render 'edit'
+      end
 
   end
 
--- capstone/config/routes.rb
+++ capstone/config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   get '/home' => 'home#home'
 
   get '/editCountdown' => 'home#edit'
+  post '/editCountdown' => 'home#updateDate'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
